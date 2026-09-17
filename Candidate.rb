c2ff70cc2792b1239b44a41032ba67ee20240f61
ThomasDebek <tom.debek@gmail.com>
2026-09-17 21:02:57 +0200

    Candidate: create :)

--- Candidate.rb
+++ Candidate.rb
@@ -19,12 +19,38 @@ class Candidate
   def total_score
     @scores.values.sum
   end
+
+  def average_score
+    return 0 if @scores.empty?
+    total_score.to_f / @scores.size
+  end
+
+  def strongest_skill
+    @scores.max_by { |skill, score| score }
+  end
+
+  def weakest_skill
+    @scores.min_by { |skill, score| score }
+  end
+
 end
 
 
-candidate = Candidate.new("Adam", "Junior Ruby Developer", {} )
+candidate = Candidate.new("Thomas", "Ruby Developer", {} )
 candidate.add_score("Ruby", 82)
 candidate.add_score("OOP", 74)
 candidate.add_score("SQL", 61)
 candidate.add_score("Debugging", 90)
 puts candidate.total_score
+
+
+strongest_skill, strongest_score = candidate.strongest_skill
+weakest_skill, weakest_score = candidate.weakest_skill
+
+puts "Candidate: #{candidate.name}"
+puts "Level: #{candidate.level}"
+puts "Scores: #{candidate.scores}"
+puts "Total score: #{candidate.total_score}"
+puts "Average score: #{candidate.average_score}"
+puts "Strongest skill: #{strongest_skill} – #{strongest_score} points"
+puts "Weakest skill: #{weakest_skill} – #{weakest_score} points"
