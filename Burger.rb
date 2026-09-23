2677c46a531861db026c544a773563d3761bd727
ThomasDebek <tom.debek@gmail.com>
2026-09-23 22:18:19 +0200

    Good price

--- Burger.rb
+++ Burger.rb
@@ -11,11 +11,20 @@ class Burger
     @stock > 0
   end
 
+  def good_price?
+    @price <= 50
+  end
+
   def sell
     if available?
-      @stock -= 1
-      puts "#{name} sold for #{price} PLN. Remaining stock: #{@stock}."
-      @price
+      if good_price?
+        @stock -= 1
+        puts "#{name} sold for #{price} PLN. Remaining stock: #{@stock}."
+        @price
+      else
+        puts "#{name} is too expensive. Price: #{price} PLN."
+        0
+      end
     else
       puts "#{name} is out of stock."
       0
@@ -29,11 +38,9 @@ class Burger
       puts "#{name} is out of stock."
     end
   end
-
 end
 
-classic_burger = Burger.new("Classic Burger", 100, 200)
-
+classic_burger = Burger.new("Classic Burger", 40, 200)
 
 classic_burger.sell
 classic_burger.info_burger
--- Order.rb
+++ Order.rb
@@ -22,3 +22,23 @@ end
 
 order = Order.new
 p order.total_paid
+
+
+module Loggable
+  def log
+    p "Logging......."
+  end
+end
+
+class User
+  include Loggable
+end
+
+class Person
+  extend Loggable
+end
+
+user = User.new
+user.log
+people = Person.new.extend(Loggable)
+people.log
